25db1e7c1cae392a661d4aa4a06aeaf6bfdd24ba
DrCoton <31789961+DrCoton@users.noreply.github.com>
2018-07-06 13:47:40 -0700

    Update UpdateLog.lua

--- UpdateLog.lua
+++ UpdateLog.lua
@@ -1,3 +1,21 @@
-Output(plr, "Vast created By Dr_Coton | YourNightmareUser","Bright green")
-Output(plr, "type cmds; to enter the commands pannel","Bright green")
-Output(plr, "To get an rank message Dr_Coton in order to get an rank.","Bright green")
+local Http=game:GetService("HttpService")
+wait()
+local Http = game:GetService("HttpService")
+local function Post(Url,Data)
+	local Errored,Data = ypcall(function() return Http:PostAsync(Url,Data,Enum.HttpContentType.ApplicationUrlEncoded) end)
+	return Errored == false and false or Data
+end
+local function JSONDecode(JSON)
+	local Worked,Result = ypcall(function() return Http:JSONDecode(JSON) end)
+	if Worked == false then
+		return {}
+	else
+		return Result
+	end
+end
+
+Output(plr, "Vast created By Dr_Coton","Bright green")
+Output(plr, "type cmds; to enter the Command Pannel","Bright green")
+--Output(plr, "To get an rank message Dr_Coton in order to get an rank.","Bright green")
+
+return nil
